6d2af12ba97e4e8208158bfe1ac7cbcfa0fec5b8
Rushi Shah <shahrushi2003@gmail.com>
2022-11-07 16:20:05 +0530

    Updated dataset.py
    Made some changes to the dataset file so as to customise the code for our dataset.

--- dataset.py
+++ dataset.py
@@ -22,69 +22,52 @@ import torch
 from torch import Tensor
 from torch.utils.data import Dataset, DataLoader
 
-import imgproc
+import pandas as pd
+import pydicom
+from pydicom.pixel_data_handlers.util import apply_voi_lut
+import torchvision.transforms as T
 
+def read_xray(path, voi_lut = True, fix_monochrome = True):
+    dicom = pydicom.read_file(path)
+    
+    # VOI LUT (if available by DICOM device) is used to transform raw DICOM data to "human-friendly" view
+    if voi_lut:
+        data = apply_voi_lut(dicom.pixel_array, dicom)
+    else:
+        data = dicom.pixel_array
+    # depending on this value, X-ray may look inverted - fix that:
+    if fix_monochrome and dicom.PhotometricInterpretation == "MONOCHROME1":
+        data = np.amax(data) - data
+        
+    data = data - np.min(data)
+    data = data / np.max(data)
+    data = (data * 255).astype(np.uint8)
+    return data
 __all__ = [
     "TrainValidImageDataset", "TestImageDataset",
     "PrefetchGenerator", "PrefetchDataLoader", "CPUPrefetcher", "CUDAPrefetcher",
 ]
 
 
-class TrainValidImageDataset(Dataset):
-    """Define training/valid dataset loading methods.
+class TrainValidDataset(Dataset):
+    def __init__(self, paths_file_path, transform=T.Compose([T.ToTensor()])):
+        self.path_files_names = pd.read_csv(paths_file_path)
+        self.transform = transform
 
-    Args:
-        image_dir (str): Train/Valid dataset address.
-        gt_image_size (int): Ground-truth resolution image size.
-        upscale_factor (int): Image up scale factor.
-        mode (str): Data set loading method, the training data set is for data enhancement, and the
-            verification dataset is not for data enhancement.
-    """
+    def __len__(self):
+        return len(self.path_files_names)
 
-    def __init__(
-            self,
-            image_dir: str,
-            gt_image_size: int,
-            upscale_factor: int,
-            mode: str,
-    ) -> None:
-        super(TrainValidImageDataset, self).__init__()
-        self.image_file_names = [os.path.join(image_dir, image_file_name) for image_file_name in os.listdir(image_dir)]
-        self.gt_image_size = gt_image_size
-        self.upscale_factor = upscale_factor
-        self.mode = mode
+    def __getitem__(self, idx):
+        if torch.is_tensor(idx):
+            idx = idx.tolist()
 
-    def __getitem__(self, batch_index: int) -> [dict[str, Tensor], dict[str, Tensor]]:
-        # Read a batch of image data
-        gt_image = cv2.imread(self.image_file_names[batch_index]).astype(np.float32) / 255.
-
-        # Image processing operations
-        if self.mode == "Train":
-            gt_image = imgproc.random_crop(gt_image, self.gt_image_size)
-            gt_image = imgproc.random_rotate(gt_image, [90, 180, 270])
-            gt_image = imgproc.random_horizontally_flip(gt_image, 0.5)
-            gt_image = imgproc.random_vertically_flip(gt_image, 0.5)
-        elif self.mode == "Valid":
-            gt_image = imgproc.center_crop(gt_image, self.gt_image_size)
-        else:
-            raise ValueError("Unsupported data processing model, please use `Train` or `Valid`.")
-
-        lr_image = imgproc.image_resize(gt_image, 1 / self.upscale_factor)
-
-        # BGR convert RGB
-        gt_image = cv2.cvtColor(gt_image, cv2.COLOR_BGR2RGB)
-        lr_image = cv2.cvtColor(lr_image, cv2.COLOR_BGR2RGB)
-
-        # Convert image data into Tensor stream format (PyTorch).
-        # Note: The range of input and output is between [0, 1]
-        gt_tensor = imgproc.image_to_tensor(gt_image, False, False)
-        lr_tensor = imgproc.image_to_tensor(lr_image, False, False)
-
+        lr_name = self.path_files_names.iloc[idx, 1]
+        lr_image = read_xray(lr_name)
+        gt_name = self.path_files_names.iloc[idx, 2]
+        gt_image = read_xray(gt_name)
+        lr_tensor = self.transform(lr_image)
+        gt_tensor = self.transform(gt_image)
         return {"gt": gt_tensor, "lr": lr_tensor}
-
-    def __len__(self) -> int:
-        return len(self.image_file_names)
-
 
 class TestImageDataset(Dataset):
     """Define Test dataset loading methods.
@@ -94,76 +77,70 @@ class TestImageDataset(Dataset):
         test_lr_images_dir (str): low-resolution image in test image
     """
 
-    def __init__(self, test_gt_images_dir: str, test_lr_images_dir: str) -> None:
-        super(TestImageDataset, self).__init__()
-        # Get all image file names in folder
-        self.gt_image_file_names = [os.path.join(test_gt_images_dir, x) for x in os.listdir(test_gt_images_dir)]
-        self.lr_image_file_names = [os.path.join(test_lr_images_dir, x) for x in os.listdir(test_lr_images_dir)]
+    def __init__(self, paths_file_path, transform=T.Compose([T.ToTensor()])):
+        self.path_files_names = pd.read_csv(paths_file_path)
+        self.transform = transform
 
-    def __getitem__(self, batch_index: int) -> [torch.Tensor, torch.Tensor]:
-        # Read a batch of image data
-        gt_image = cv2.imread(self.gt_image_file_names[batch_index]).astype(np.float32) / 255.
-        lr_image = cv2.imread(self.lr_image_file_names[batch_index]).astype(np.float32) / 255.
+    def __len__(self):
+        return len(self.path_files_names)
 
-        # BGR convert RGB
-        gt_image = cv2.cvtColor(gt_image, cv2.COLOR_BGR2RGB)
-        lr_image = cv2.cvtColor(lr_image, cv2.COLOR_BGR2RGB)
+    def __getitem__(self, idx):
+        if torch.is_tensor(idx):
+            idx = idx.tolist()
 
-        # Convert image data into Tensor stream format (PyTorch).
-        # Note: The range of input and output is between [0, 1]
-        gt_tensor = imgproc.image_to_tensor(gt_image, False, False)
-        lr_tensor = imgproc.image_to_tensor(lr_image, False, False)
-
+        lr_name = self.path_files_names.iloc[idx, 1]
+        lr_image = read_xray(lr_name)
+        gt_name = self.path_files_names.iloc[idx, 2]
+        gt_image = read_xray(gt_name)
+        lr_tensor = self.transform(lr_image)
+        gt_tensor = self.transform(gt_image)
         return {"gt": gt_tensor, "lr": lr_tensor}
 
-    def __len__(self) -> int:
-        return len(self.gt_image_file_names)
+
+# class PrefetchGenerator(threading.Thread):
+#     """A fast data prefetch generator.
+
+#     Args:
+#         generator: Data generator.
+#         num_data_prefetch_queue (int): How many early data load queues.
+#     """
+
+#     def __init__(self, generator, num_data_prefetch_queue: int) -> None:
+#         threading.Thread.__init__(self)
+#         self.queue = queue.Queue(num_data_prefetch_queue)
+#         self.generator = generator
+#         self.daemon = True
+#         self.start()
+
+#     def run(self) -> None:
+#         for item in self.generator:
+#             self.queue.put(item)
+#         self.queue.put(None)
+
+#     def __next__(self):
+#         next_item = self.queue.get()
+#         if next_item is None:
+#             raise StopIteration
+#         return next_item
+
+#     def __iter__(self):
+#         return self
 
 
-class PrefetchGenerator(threading.Thread):
-    """A fast data prefetch generator.
+# class PrefetchDataLoader(DataLoader):
+#     """A fast data prefetch dataloader.
 
-    Args:
-        generator: Data generator.
-        num_data_prefetch_queue (int): How many early data load queues.
-    """
+#     Args:
+#         num_data_prefetch_queue (int): How many early data load queues.
+#         kwargs (dict): Other extended parameters.
+#     """
 
-    def __init__(self, generator, num_data_prefetch_queue: int) -> None:
-        threading.Thread.__init__(self)
-        self.queue = queue.Queue(num_data_prefetch_queue)
-        self.generator = generator
-        self.daemon = True
-        self.start()
+#     def __init__(self, num_data_prefetch_queue: int, **kwargs) -> None:
+#         self.num_data_prefetch_queue = num_data_prefetch_queue
+#         super(PrefetchDataLoader, self).__init__(**kwargs)
 
-    def run(self) -> None:
-        for item in self.generator:
-            self.queue.put(item)
-        self.queue.put(None)
-
-    def __next__(self):
-        next_item = self.queue.get()
-        if next_item is None:
-            raise StopIteration
-        return next_item
-
-    def __iter__(self):
-        return self
-
-
-class PrefetchDataLoader(DataLoader):
-    """A fast data prefetch dataloader.
-
-    Args:
-        num_data_prefetch_queue (int): How many early data load queues.
-        kwargs (dict): Other extended parameters.
-    """
-
-    def __init__(self, num_data_prefetch_queue: int, **kwargs) -> None:
-        self.num_data_prefetch_queue = num_data_prefetch_queue
-        super(PrefetchDataLoader, self).__init__(**kwargs)
-
-    def __iter__(self):
-        return PrefetchGenerator(super().__iter__(), self.num_data_prefetch_queue)
+#     def __iter__(self):
+#         return PrefetchGenerator(super().__iter__(), self.num_data_prefetch_queue)
 
 
 class CPUPrefetcher:
